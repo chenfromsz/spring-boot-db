368dc8ed3dfe2b8687003e9c64d34db438e53d35
chensj <chensj@hyxt.com>
2016-12-05 15:28:30 +0800

    update Spring Boot from 1.3.2 to 1.4.2 update Spring Data Neo4j from 4.0.0 to 4.1.5

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Update Spring Boot from 1.3.2 to 1.4.2
--- neo4j/src/main/java/dbdemo/neo4j/config/Neo4jConfig.java
+++ neo4j/src/main/java/dbdemo/neo4j/config/Neo4jConfig.java
@@ -13,10 +13,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableNeo4jRepositories(basePackages = { "dbdemo.neo4j.repositories" })
 public class Neo4jConfig extends Neo4jConfiguration {
-    @Override
-    public Neo4jServer neo4jServer() {
-        return new RemoteServer("http://192.168.1.221:7474","neo4j","12345678");
-    }
+//SDN 升级到4.1.5，连接服务器的配置改在ogm.properties中设定，这样可以访问Neo4j 2.x 到 3.x 版本
+//    @Override
+//    public Neo4jServer neo4jServer() {
+//        return new RemoteServer("http://192.168.1.221:7474","neo4j","12345678");
+//    }
 
     @Override
     public SessionFactory getSessionFactory() {
