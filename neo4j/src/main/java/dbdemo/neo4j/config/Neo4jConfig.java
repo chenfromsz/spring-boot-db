a2316871c3d94c30f256b52e78b643a76f3415d6
chensj <chensj@hyxt.com>
2016-12-06 11:13:42 +0800

    update Spring Boot to 1.4.2

--- neo4j/src/main/java/dbdemo/neo4j/config/Neo4jConfig.java
+++ neo4j/src/main/java/dbdemo/neo4j/config/Neo4jConfig.java
@@ -4,8 +4,6 @@ import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.server.Neo4jServer;
-import org.springframework.data.neo4j.server.RemoteServer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
